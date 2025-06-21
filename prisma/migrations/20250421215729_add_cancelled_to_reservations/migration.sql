777acc2206ea63e80f295dc73cb1e863a845c551
Ezequiel Arevalo <ezequiel.d.arevalo@gmail.com>
2025-06-21 23:03:14 +0000

    migrations from zero

--- prisma/migrations/20250421215729_add_cancelled_to_reservations/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Reservation" ADD COLUMN     "cancelled" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20250427221558_add_payment_fields_to_reservation/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateEnum
-CREATE TYPE "PaymentStatus" AS ENUM ('pending', 'partial', 'paid');
-
--- AlterTable
-ALTER TABLE "Reservation" ADD COLUMN     "amountPaid" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "paymentStatus" "PaymentStatus" NOT NULL DEFAULT 'pending',
-ALTER COLUMN "updatedAt" SET DEFAULT CURRENT_TIMESTAMP;
